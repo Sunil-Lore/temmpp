12c39cd82f38b81812d0fc9c92b237f174b2e92b
Sunil Kumar <sunil.lore@outlook.com>
2024-02-16 21:50:17 +0530

    updated latest getcss

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php #index.php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/getcss.min.css" />
+  <link rel="stylesheet" href="scss/custom.css" />
+  <title>AutoXpo</title>
+</head>
+<body>
+  <div class="container ">
+  <div class="row">
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste iusto perferendis quis? Veritatis perferendis minus soluta, amet ipsam consequuntur nulla quo dolor corporis nisi dolores distinctio, magni officia. Velit deserunt autem aliquid voluptates ipsum sequi magni rerum dolore, laboriosam commodi! Quibusdam ut ipsum distinctio veniam cupiditate officiis aut dolor sed!</p>
+    </div>
+  </div>
+  </div>
+</body>
+</html>
